093fa9a9149ee7bb9098d7bc9d2426bd649e661a
brover <github@yosiket.xyz>
2023-05-28 20:42:46 +0700

    Add files via upload

---
+++ assets/js/warn.js
@@ -0,0 +1,7 @@
+console.log('%cWARNING', 'font-size:10em;color:red;')
+console.log(`%cนี่เป็นคุณลักษณะเบราว์เซอร์สำหรับนักพัฒนาซอฟต์แวร์
+อย่าคัดลอกและวางบางสิ่งที่นี่หากคุณไม่เข้าใจ
+
+สามารถศึกษาเพิ่มเติมได้ที่:
+https://en.wikipedia.org/wiki/Self-XSS`,
+'font-size:2em')
